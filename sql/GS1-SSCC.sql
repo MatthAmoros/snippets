20c1bb654629c9b83e9494a1aa0a8497c7c5f099
SunPaz <matth.amoros@gmail.com>
2018-04-24 17:24:10 -0300

    Update GS1-SSCC.sql

--- sql/GS1-SSCC.sql
+++ sql/GS1-SSCC.sql
@@ -1,4 +1,6 @@
-/****** Object:  StoredProcedure [dbo].[SEQ_NEXT_ID]    Script Date: 24/04/2018 16:00:00 ******/
+USE [TEST]
+GO
+/****** Object:  StoredProcedure [dbo].[SEQ_NEXT_ID]    Script Date: 24/04/2018 17:20:15 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -6,16 +8,20 @@ GO
 -- =============================================
 -- Author:		Matthieu
 -- Create date: 28-02-2018
--- Description:	Return an SSCC
+-- Description:	Devuelve un identificator unico
 -- =============================================
 ALTER PROCEDURE [dbo].[SEQ_NEXT_ID] 
-@PREFIX NVARCHAR(8)
+@PREFIJO NVARCHAR(10),
+@SSCC_WHITHOUT_CD NVARCHAR(17) = NULL,
+@SSCC_CD NVARCHAR(1) = NULL
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    --PREFIX = [1][780][465287] => [1 - 5][COUNTRY CODE][COMPANY CODE]
-	  SELECT  FORMAT((NEXT VALUE FOR dbo.INCREMENTS_PALLET),CONCAT(@PREFIX, '##########')) AS Id;  
+    -- Insert statements for procedure here
+	  SET @SSCC_WHITHOUT_CD = (FORMAT((NEXT VALUE FOR dbo.CORRELATIVO_LOTES),CONCAT(@PREFIJO, '#######')));
+	  set @SSCC_CD = (SELECT [dbo].[get_check_digit_gs1_sscc] (@SSCC_WHITHOUT_CD));
+	  SELECT CONCAT(@SSCC_WHITHOUT_CD, @SSCC_CD);
 END
