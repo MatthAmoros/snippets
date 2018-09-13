e1cde34175ec92f84c57d34df328a9365daeea54
SunPaz <matth.amoros@gmail.com>
2018-09-13 08:16:31 -0700

    Update config.py

--- python/flask-template/instance/config.py
+++ python/flask-template/instance/config.py
@@ -1,1 +1,7 @@
 """ Instance configuration variables """
+""" This file contains configuration variables that shouldn’t be in version control. 
+This includes things like API keys and database URIs containing passwords. 
+This also contains variables that are specific to this particular instance of your application. 
+For example, you might have DEBUG = False in config.py, but set DEBUG = True in instance/config.py on your local
+machine for development. Since this file will be read in after config.py, it will override it and set DEBUG = True.
+"""
