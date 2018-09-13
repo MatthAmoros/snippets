cc90290e756edeb49967eb047cd16d37d8a474fa
SunPaz <matth.amoros@gmail.com>
2018-09-13 08:23:02 -0700

    Create run.py

---
+++ python/flask-template/run.py
@@ -0,0 +1,3 @@
+""" This is the file that is invoked to start up a development server.
+It gets a copy of the app from your package and runs it.
+This won’t be used in production, but it will see a lot of mileage in development. """
